a4c41c73b1f124e790376515d3229f5149dea7ee
Guilherme A. Melo <guilhermearmelo@gmail.com>
2018-12-13 23:07:02 -0200

    Update connect.py

--- connect.py
+++ connect.py
@@ -3,15 +3,18 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+# Definição de conexões com o banco de dados
 app.config['MONGO_DBNAME'] = 'desafioditoconnect'
 app.config['MONGO_URI'] = 'mongodb://guilhermemelo:guidll2@ds029187.mlab.com:29187/desafioditoconnect'
 
 mongo = PyMongo(app)
 
+# Definição da página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# CREATE
 @app.route('/add', methods=['POST'])
 def adicionar():
     event = request.form['novoevent']
@@ -20,12 +23,14 @@ def adicionar():
     dado.insert({'event': event, 'timestamp': timestamp})
     return render_template('add.html', novoevento=event)
 
+# READ
 @app.route('/find')
 def find():
     dado = mongo.db.dados
     x = dado.find_one({'event': 'Evento1'})
     return 'Evento Encontrado!'
 
+# UPDATE
 @app.route('/update')
 def update():
     dado = mongo.db.dados
@@ -34,6 +39,7 @@ def update():
     dado.save(x)
     return 'Evento Atualizado!'
 
+# DELETE
 @app.route('/delete')
 def delete():
     dado = mongo.db.users
